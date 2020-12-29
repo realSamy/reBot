83b09b87eecdc78d4c6c507dbaaf3bb24f010570
Saman Hoodaji <59841686+realSamy@users.noreply.github.com>
2020-12-29 23:57:38 +0330

    Delete autoload.php

--- src/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php /** @noinspection PhpIncludeInspection */
-spl_autoload_register(static function ($name) {
-    $path = __DIR__ . '/' . str_replace(['\\', 'realSamy/'], ['/', ''], $name) . '.php';
-    if (file_exists($path)) {
-        include $path;
-    }
-});
